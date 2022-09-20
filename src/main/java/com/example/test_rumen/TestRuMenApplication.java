9072d9dfcd206dd3335188af04fae498dc0b7e3c
1643953546 <50839050+1643953546@users.noreply.github.com>
2022-09-20 21:52:25 +0800

    Update TestRuMenApplication.java

--- src/main/java/com/example/test_rumen/TestRuMenApplication.java
+++ src/main/java/com/example/test_rumen/TestRuMenApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestRuMenApplication {
-    //
+    //1
     public static void main(String[] args) {
         SpringApplication.run(TestRuMenApplication.class, args);
     }
